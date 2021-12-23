b4c6f835c35a05c42ce0f6467d386bcfe6ffc008
Ajalaya Nakin <644230031@webmail.npru.ac.th>
2021-12-23 15:23:16 +0700

    first commit

---
+++ _01_MyfirstProgram.java
@@ -0,0 +1,12 @@
+package week01;
+
+class _01_MyfirstProgram {
+    public static void main(String [] args){
+        System.out.println("Welcome to Java Program");
+        System.out.println("====================");
+        System.out.println("My name is Ajalaya Nakin");
+        System.out.println("Age 21");
+        System.out.println("Unibersity : NPRU");
+
+    }
+}
